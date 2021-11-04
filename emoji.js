dac07b18a70905c77438d8c3b14fbe1516a9b527
Ercan Ersoy <ercanersoy@reklam5.com>
2021-11-04 20:02:35 +0300

    Emoji

---
+++ emoji.js
@@ -0,0 +1,13 @@
+// Get emojis from https://emojipedia.org/
+
+const btn = document.getElementById("emoji-btn");
+const emojis = [ 
+    "😆",  "😅",  "🤣",  "😂",  "😀",  "🤑",  "🤨",  "🙂",
+    "😊",  "😗",  "😛",  "😏",  "🤥",  "😴",  "🥺", "😧",
+    "🤗",  "🤩",  "😎",  "🥳",  "😍",  "😱",  "🤓", "😷",
+    "🥴",  "😳",  "🤯",  "🤫",  "🤑",  "😪",  "😴", "😵","😘","🥰"
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+});
